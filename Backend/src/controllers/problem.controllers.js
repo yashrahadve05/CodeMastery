5415b062ab674a456b37c3d71d159d277542cae6
yashrahadve <yashrahadve05@gmail.com>
2025-05-16 01:29:40 +0530

    Add getAllProblemsSolvedByUser controller to fetch problems solved by the user

--- Backend/src/controllers/problem.controllers.js
+++ Backend/src/controllers/problem.controllers.js
@@ -286,4 +286,38 @@ export const deleteProblem = async (req, res) => {
     }
 };
 
-export const getAllProblemsSolvedByUser = async (req, res) => {};
+export const getAllProblemsSolvedByUser = async (req, res) => {
+    try {
+        const problems = await db.problem.findMany({
+            where:{
+                solvedBy: {
+                    some:{
+                        userId: req.user.id
+                    }
+                }
+            },
+            include: {
+                solvedBy: {
+                    where: {
+                        userId: req.user.id
+                    }
+                }
+            }
+        })
+
+
+        res.status(200).json({
+            success: true,
+            message: "Problems fetched successfully",
+            problems
+        })
+
+    } catch (error) {
+        console.error("Error fetching problems: ", error);
+        res.status(500).json({
+            success: false,
+            message: "Error fetching problems!",
+            error: error
+        })
+    }
+};
