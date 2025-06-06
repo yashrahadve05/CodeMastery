19eefd1a6fe8656f4adac379e2078362e1d270ee
yashrahadve05 <yashrahadve05@gmail.com>
2025-06-06 20:16:18 +0530

    Refactor CORS configuration to support multiple allowed origins

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,10 +13,20 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://code-mastery-opal.vercel.app",
+];
 app.use(
     cors({
-        // origin: "http://localhost:5173",
-        origin: "https://code-mastery-opal.vercel.app",
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
