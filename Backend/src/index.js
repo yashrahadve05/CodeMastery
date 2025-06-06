7d6216afca5a5a45e65c2c137b07d0a032560b91
yashrahadve05 <yashrahadve05@gmail.com>
2025-06-06 19:14:32 +0530

    Update CORS origin to allow requests from the production URL

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://code-mastery-opal.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
