f53076bcb2b6da93ce08f2248a2f65fe14a199c0
yashrahadve05 <yashrahadve05@gmail.com>
2025-06-07 15:04:15 +0530

    Add allowed origin for CORS to support new deployment URL

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -17,6 +17,7 @@ app.use(cookieParser());
 const allowedOrigins = [
     "http://localhost:5173",
     "https://code-mastery-opal.vercel.app",
+    "https://codemastery-kyw9.onrender.com",
 ];
 app.use(
     cors({
