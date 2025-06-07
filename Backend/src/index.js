f375c8d9157d2891ade8d4f2e74f8b94c5de868a
yashrahadve05 <yashrahadve05@gmail.com>
2025-06-07 15:28:53 +0530

    Add CORS headers to support specific origin and methods

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -33,6 +33,19 @@ app.use(
     })
 );
 
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://code-mastery-opal.vercel.app"
+    ); // Or '*' to allow any origin
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow common methods
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    ); // Allow common headers
+    next();
+});
+
 app.get("/yash", (req, res) => {
     try {
         res.send({
