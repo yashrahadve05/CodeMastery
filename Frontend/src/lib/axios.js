2f7b8f24ed24bb2ea7fcf35e868edf15eb86af5a
yashrahadve05 <yashrahadve05@gmail.com>
2025-06-07 15:00:36 +0530

    Update axios baseURL for production environment

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "https://codemastery-kyw9.onrender.com/api/v1",
     withCredentials: true,
     
 })
